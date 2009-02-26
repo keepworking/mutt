41a22747e6660b0453c9344531376492c0c35e51
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:25:35 +0000

    [project @ e8412142a10d0431cec4b606b3deaf2b6b5d9c7d] Vikas' latest collapse fix.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-02-22 12:37:43+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@876 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- globals.h
+++ globals.h
@@ -22,6 +22,10 @@ WHERE CONTEXT *Context;
 
 WHERE char Errorbuf[SHORT_STRING];
 
+#if defined(DL_STANDALONE) && defined(USE_DOTLOCK)
+WHERE char *MuttDotlock;
+#endif
+
 WHERE char *AliasFile;
 WHERE char *AliasFmt;
 WHERE char *AttachSep;
@@ -52,6 +56,12 @@ WHERE char *Locale;
 WHERE char *MailcapPath;
 WHERE char *Maildir;
 WHERE char *MsgFmt;
+
+#ifdef MIXMASTER
+WHERE char *Mixmaster;
+WHERE char *MixEntryFormat;
+#endif
+
 WHERE char *Muttrc INITVAL (NULL);
 WHERE char *Outbox;
 WHERE char *Pager;
@@ -68,6 +78,7 @@ WHERE char *Prefix;
 WHERE char *PrintCmd;
 WHERE char *QueryCmd;
 WHERE char *Realname;
+WHERE char *SendCharset;
 WHERE char *Sendmail;
 WHERE char *Shell;
 WHERE char *Signature;
