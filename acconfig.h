b40124223a3e6a76d73a2a63f26967ffed169474
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:53:03 +0000

    [project @ ed70e8447bb324e449dd7565717e15a21aa82efb] Ignore Makefile.in, Makefile, and .deps.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 2000-05-12 08:06:49+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@1589 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- acconfig.h
+++ acconfig.h
@@ -12,7 +12,7 @@
 #undef MUTTLOCALEDIR
 
 /* Enable debugging info */
-#undef DEBUG
+#define DEBUG
 
 /* What is your domain name? */
 #undef DOMAIN
@@ -98,6 +98,9 @@
 /* The "buffy_size" feature */
 #undef BUFFY_SIZE
 
+/* The Sun mailtool attachments support */
+#undef SUN_ATTACHMENT 
+
 /* The result of isprint() is unreliable? */
 #undef LOCALES_HACK
 
@@ -172,3 +175,7 @@
 /* Define if you have resizeterm, as a function or macro.  */
 #undef HAVE_RESIZETERM
 
+/* Some systems declare sig_atomic_t as volatile, smome others -- no.
+ * This define will have value `sig_atomic_t' or `volatile sig_atomic_t'
+ * accordingly. */
+#undef SIG_ATOMIC_VOLATILE_T
