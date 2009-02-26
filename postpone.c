b2ae1c135a1f8aa14fa4f5625ad440ca28c5fc91
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 23:22:34 +0000

    [project @ 11d98a7089ddf2f2cd5f65abc66282155277c1a1] Add new m4 files from patch.1.3.28.lh.gettext-0.11.1.
    Original author: Lars Hecking <lhecking@nmrc.ie>
    Date: 2002-04-20 07:24:54+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@2440 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- postpone.c
+++ postpone.c
@@ -47,10 +47,6 @@ static struct mapping_t PostponeHelp[] = {
 #include "pgp.h"
 #endif /* HAVE_PGP */
 
-#ifdef HAVE_SMIME
-#include "smime.h"
-#endif /* HAVE_SMIME */
-
 
 static short PostCount = 0;
 static CONTEXT *PostContext = NULL;
@@ -338,8 +334,7 @@ int mutt_get_postponed (CONTEXT *ctx, HEADER *hdr, HEADER **cur, char *fcc, size
 						       */
 	     || mutt_strncmp ("X-Mutt-PGP:", tmp->data, 11) == 0)
     {
-      hdr->security = mutt_parse_crypt_hdr (strchr (tmp->data, ':') + 1, 1);
-      hdr->security |= APPLICATION_PGP;
+      hdr->pgp = mutt_parse_pgp_hdr (strchr (tmp->data, ':') + 1, 1);
        
       /* remove the pgp field */
       next = tmp->next;
@@ -352,26 +347,6 @@ int mutt_get_postponed (CONTEXT *ctx, HEADER *hdr, HEADER **cur, char *fcc, size
       tmp = next;
     }
 #endif /* HAVE_PGP */
-
-
-#ifdef HAVE_SMIME
-    else if (mutt_strncmp ("X-Mutt-SMIME:", tmp->data, 13) == 0)
-    {
-      hdr->security = mutt_parse_crypt_hdr (strchr (tmp->data, ':') + 1, 1);
-      hdr->security |= APPLICATION_SMIME;
-       
-      /* remove the smime field */
-      next = tmp->next;
-      if (last)
-	last->next = tmp->next;
-      else
-	hdr->env->userhdrs = tmp->next;
-      tmp->next = NULL;
-      mutt_free_list (&tmp);
-      tmp = next;
-    }
-#endif /* HAVE_SMIME */
-
 
 #ifdef MIXMASTER
     else if (mutt_strncmp ("X-Mutt-Mix:", tmp->data, 11) == 0)
@@ -408,13 +383,12 @@ int mutt_get_postponed (CONTEXT *ctx, HEADER *hdr, HEADER **cur, char *fcc, size
 
 
 
-#if defined(HAVE_PGP) || defined(HAVE_SMIME)
+#ifdef HAVE_PGP
 
-int mutt_parse_crypt_hdr (char *p, int set_signas)
+int mutt_parse_pgp_hdr (char *p, int set_signas)
 {
   int pgp = 0;
   char pgp_sign_as[LONG_STRING] = "\0", *q;
-  char smime_cryptalg[LONG_STRING] = "\0";
    
   SKIPWS (p);
   for (; *p; p++)
@@ -424,12 +398,12 @@ int mutt_parse_crypt_hdr (char *p, int set_signas)
     {
       case 'e':
       case 'E':
-        pgp |= ENCRYPT;
+        pgp |= PGPENCRYPT;
         break;
 
       case 's':    
       case 'S':
-        pgp |= SIGN;
+        pgp |= PGPSIGN;
         q = pgp_sign_as;
       
         if (*(p+1) == '<')
@@ -469,27 +443,6 @@ int mutt_parse_crypt_hdr (char *p, int set_signas)
 
 	break;
 	  
-	  
-      case 'c':
-      case 'C':
-   	q = smime_cryptalg;
-	
-        if(*(p+1) == '<')
-	{
-	  for(p += 2; *p && *p != '>' && q < smime_cryptalg + sizeof(smime_cryptalg) - 1;
-	      *q++ = *p++)
-	    ;
-	  
-	  if(*p != '>')
-	  {
-	    mutt_error _("Illegal S/MIME header");
-	    return 0;
-	  }
-	}
-
-	*q = '\0';
-	break;
-
       default:
         mutt_error _("Illegal PGP header");
         return 0;
@@ -497,22 +450,12 @@ int mutt_parse_crypt_hdr (char *p, int set_signas)
      
   }
  
-  /* the cryptalg field must not be empty */
-#ifdef HAVE_SMIME
-  if (*smime_cryptalg)
-    mutt_str_replace (&SmimeCryptAlg, smime_cryptalg);
-#endif /*  HAVE_SMIME */
-
-#ifdef HAVE_PGP
   if (set_signas || *pgp_sign_as)
     mutt_str_replace (&PgpSignAs, pgp_sign_as);
-#endif /* HAVE_PGP */
 
   return pgp;
 }
-#endif /* HAVE_PGP ||  HAVE_SMIME */
-
-
+#endif /* HAVE_PGP */
 
 int mutt_prepare_template (FILE *fp, CONTEXT *ctx, HEADER *newhdr, HEADER *hdr,
 			       short weed)
@@ -566,10 +509,10 @@ int mutt_prepare_template (FILE *fp, CONTEXT *ctx, HEADER *newhdr, HEADER *hdr,
 
 #ifdef HAVE_PGP
   /* decrypt pgp/mime encoded messages */
-  if ((hdr->security & APPLICATION_PGP) && 
+  if ((hdr->pgp & PGPENCRYPT) && 
       mutt_is_multipart_encrypted (newhdr->content))
   {
-    newhdr->security |= PGPENCRYPT;
+    newhdr->pgp |= PGPENCRYPT;
     if (!pgp_valid_passphrase())
       goto err;
 
@@ -588,9 +531,6 @@ int mutt_prepare_template (FILE *fp, CONTEXT *ctx, HEADER *newhdr, HEADER *hdr,
 
     mutt_clear_error ();
   }
-#endif
-
-#if defined(HAVE_PGP)|| defined(HAVE_SMIME)
 
   /* 
    * remove a potential multipart/signed layer - useful when
@@ -599,8 +539,8 @@ int mutt_prepare_template (FILE *fp, CONTEXT *ctx, HEADER *newhdr, HEADER *hdr,
   
   if (mutt_is_multipart_signed (newhdr->content))
   {
-    newhdr->security |= SIGN;
-
+    newhdr->pgp |= PGPSIGN;
+    
     /* destroy the signature */
     mutt_free_body (&newhdr->content->parts->next);
     newhdr->content = mutt_remove_multipart (newhdr->content);
