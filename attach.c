19f8cdf1515fb05717ed96108115a43f8cc4ee21
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:21:17 +0000

    [project @ c7ed06d17016cfb17b5d61751e8f95e130b880d9] Brandon's imap_create patch.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-02-02 11:22:39+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@785 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- attach.c
+++ attach.c
@@ -409,7 +409,7 @@ int mutt_view_attachment (FILE *fp, BODY *a, int flag)
     if (fp)
     {
       fname = safe_strdup (a->filename);
-      mutt_sanitize_filename (fname, 1);
+      mutt_sanitize_filename (fname);
     }
     else
       fname = a->filename;
@@ -707,11 +707,9 @@ int mutt_save_attachment (FILE *fp, BODY *m, char *path, int flags, HEADER *hdr)
       
       memset (&s, 0, sizeof (s));
       if (flags == M_SAVE_APPEND)
-	s.fpout = fopen (path, "a");
-      else if (flags == M_SAVE_OVERWRITE)
-	s.fpout = fopen (path, "w");
+	s.fpout = safe_fopen (path, "a");
       else
-	s.fpout = safe_fopen (path, "w");
+	s.fpout = fopen (path, "w");
       if (s.fpout == NULL)
       {
 	mutt_perror ("fopen");
@@ -773,12 +771,9 @@ int mutt_decode_save_attachment (FILE *fp, BODY *m, char *path,
   s.flags = displaying ? M_DISPLAY : 0;
 
   if (flags == M_SAVE_APPEND)
-    s.fpout = fopen (path, "a");
-  else if (flags == M_SAVE_OVERWRITE)
-    s.fpout = fopen (path, "w");
+    s.fpout = safe_fopen (path, "a");
   else
-    s.fpout = safe_fopen (path, "w");
-
+    s.fpout = fopen (path, "w");
   if (s.fpout == NULL)
   {
     perror ("fopen");
