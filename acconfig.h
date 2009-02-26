1da2d5a680e733ffb650a36863712769a46e1d6c
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:31:13 +0000

    [project @ 2b8076b9a0141a9ef5f0f64f7e93ba8a3cb36996] Latest IMAP fixes from Brandon.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-05-18 19:22:37+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@996 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- acconfig.h
+++ acconfig.h
@@ -17,9 +17,6 @@
 /* Does your version of PGP support the PGPPASSFD environment variable? */
 #define HAVE_PGPPASSFD
 
-/* Disable the X-Mailer header? */
-#undef NO_XMAILER
-
 /* What is your domain name? */
 #undef DOMAIN
 
@@ -28,6 +25,9 @@
 
 /* use dotlocking to lock mailboxes? */
 #undef USE_DOTLOCK
+
+/* use an external dotlocking program? */
+#undef DL_STANDALONE
 
 /* use flock() to lock mailboxes? */
 #undef USE_FLOCK
@@ -138,8 +138,9 @@
 /* Use the included regex.c? */
 #undef USE_GNU_REGEX
 
-/* Do we have gpgm installed? */
-#undef HAVE_GPGM
+/* Where's mixmaster located? */
+#undef MIXMASTER
 
-/* Define to `int' if <signal.h> doesn't define.  */
-#undef sig_atomic_t
+/* Where are the character set definitions located? */
+#undef CHARMAPS_DIR
+
