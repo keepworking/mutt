dac1d9c1e4458e6e9d3d6c0d8a0217d6263672d2
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:17:54 +0000

    [project @ cbac55f3ac4d78b2ed2176d2d49d2577e0ce5bc9] HP/UX portability patch from Lars Hecking.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-01-19 11:48:11+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@701 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- send.c
+++ send.c
@@ -38,9 +38,6 @@
 #include "pgp.h"
 #endif
 
-#ifdef MIXMASTER
-#include "remailer.h"
-#endif
 
 
 static void append_signature (FILE *f)
@@ -796,19 +793,13 @@ static int send_message (HEADER *msg)
   char tempfile[_POSIX_PATH_MAX];
   FILE *tempfp;
   int i;
-  
+
   /* Write out the message in MIME form. */
   mutt_mktemp (tempfile);
   if ((tempfp = safe_fopen (tempfile, "w")) == NULL)
     return (-1);
 
-#ifdef MIXMASTER
-  mutt_write_rfc822_header (tempfp, msg->env, msg->content, 0, msg->chain ? 1 : 0);
-#endif
-#ifndef MIXMASTER
-  mutt_write_rfc822_header (tempfp, msg->env, msg->content, 0, 0);
-#endif
-  
+  mutt_write_rfc822_header (tempfp, msg->env, msg->content, 0);
   fputc ('\n', tempfp); /* tie off the header. */
 
   if ((mutt_write_mime_body (msg->content, tempfp) == -1))
@@ -824,11 +815,6 @@ static int send_message (HEADER *msg)
     unlink (tempfile);
     return (-1);
   }
-
-#ifdef MIXMASTER
-  if (msg->chain)
-    return mix_send_message (msg->chain, tempfile);
-#endif
 
   i = mutt_invoke_sendmail (msg->env->to, msg->env->cc, msg->env->bcc,
 		       tempfile, (msg->content->encoding == ENC8BIT));
