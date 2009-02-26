e4b69f1e4cb9cbd854722956131ace5a6b29585d
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:02:10 +0000

    [project @ 7741d9d8fed5996b94bfb104e3688297acaf5d5c] _Really_ fix an unset or too short StChars varialbe.  (From Vikas.)
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1998-09-29 15:40:01+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@281 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- pattern.c
+++ pattern.c
@@ -824,6 +824,21 @@ mutt_pattern_exec (struct pattern_t *pat, pattern_exec_flag flags, CONTEXT *ctx,
   return (-1);
 }
 
+static void quote_simple(char *tmp, size_t len, const char *p)
+{
+  int i = 0;
+  
+  tmp[i++] = '"';
+  while (*p && i < len - 2)
+  {
+    if (*p == '\\' || *p == '"')
+      tmp[i++] = '\\';
+    tmp[i++] = *p++;
+  }
+  tmp[i++] = '"';
+  tmp[i] = 0;
+}
+  
 /* convert a simple search into a real request */
 void mutt_check_simple (char *s, size_t len, const char *simple)
 {
@@ -853,31 +868,20 @@ void mutt_check_simple (char *s, size_t len, const char *simple)
       strfcpy (s, "~U", len);
     else
     {
-      const char *p = s;
-      int i = 0;
-
-      tmp[i++] = '"';
-      while (*p && i < sizeof (tmp) - 2)
-      {
-	if (*p == '\\' || *p == '"')
-	  tmp[i++] = '\\';
-	tmp[i++] = *p++;
-      }
-      tmp[i++] = '"';
-      tmp[i] = 0;
+      quote_simple (tmp, sizeof(tmp), s);
       mutt_expand_fmt (s, len, simple, tmp);
     }
   }
 }
 
-int mutt_pattern_func (int op, char *prompt, HEADER *hdr)
+int mutt_pattern_func (int op, char *prompt)
 {
   pattern_t *pat;
   char buf[LONG_STRING] = "", *simple, error[STRING];
   BUFFER err;
   int i;
 
-  if (mutt_get_field (prompt, buf, sizeof (buf), 0) != 0 || !buf[0])
+  if (mutt_get_field (prompt, buf, sizeof (buf), M_PATTERN) != 0 || !buf[0])
     return (-1);
 
   mutt_message ("Compiling search pattern...");
@@ -899,12 +903,18 @@ int mutt_pattern_func (int op, char *prompt, HEADER *hdr)
   if (op == M_LIMIT)
   {
     for (i = 0; i < Context->msgcount; i++)
+    {
       Context->hdrs[i]->virtual = -1;
+      Context->hdrs[i]->limited = 0;
+      Context->hdrs[i]->collapsed = 0;
+      Context->hdrs[i]->num_hidden = 0;
+    }
     Context->vcount = 0;
     Context->vsize = 0;
+    Context->collapsed = 0;
   }
 
-#define this_body Context->hdrs[i]->content
+#define THIS_BODY Context->hdrs[i]->content
 
   for (i = 0; i < Context->msgcount; i++)
     if (mutt_pattern_exec (pat, M_MATCH_FULL_ADDRESS, Context, Context->hdrs[i]))
@@ -925,19 +935,21 @@ int mutt_pattern_func (int op, char *prompt, HEADER *hdr)
 	  break;
 	case M_LIMIT:
 	  Context->hdrs[i]->virtual = Context->vcount;
+	  Context->hdrs[i]->limited = 1;
 	  Context->v2r[Context->vcount] = i;
 	  Context->vcount++;
-	  Context->vsize+=this_body->length + this_body->offset -
-	                  this_body->hdr_offset;
+	  Context->vsize+=THIS_BODY->length + THIS_BODY->offset -
+	                  THIS_BODY->hdr_offset;
 	  break;
       }
     }
-#undef this_body
+#undef THIS_BODY
 
   mutt_clear_error ();
 
   if (op == M_LIMIT)
   {
+    Context->collapsed = 0;
     safe_free ((void **) &Context->pattern);
     if (Context->limit_pattern) 
       mutt_pattern_free (&Context->limit_pattern);
@@ -948,6 +960,9 @@ int mutt_pattern_func (int op, char *prompt, HEADER *hdr)
       for (i = 0; i < Context->msgcount; i++)
       {
 	Context->hdrs[i]->virtual = i;
+	Context->hdrs[i]->limited = 0;
+	Context->hdrs[i]->num_hidden = 0;
+	Context->hdrs[i]->collapsed = 0;
 	Context->v2r[i] = i;
       }
 
@@ -979,7 +994,7 @@ int mutt_search_command (int cur, int op)
   {
     strfcpy (buf, LastSearch, sizeof (buf));
     if (mutt_get_field ((op == OP_SEARCH) ? "Search for: " : "Reverse search for: ",
-		      buf, sizeof (buf), M_CLEAR) != 0 || !buf[0])
+		      buf, sizeof (buf), M_CLEAR | M_PATTERN) != 0 || !buf[0])
       return (-1);
 
     if (op == OP_SEARCH)
