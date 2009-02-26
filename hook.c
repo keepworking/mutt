4ac1ecbeaf29d638ab0c9b9ef43ea26f7d625e23
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 21:58:49 +0000

    [project @ 36683c0b27d14dfe3f4a1d85dc8c007dbb7378ec] Another fix from Liviu.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1998-07-22 22:47:57+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@211 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- hook.c
+++ hook.c
@@ -196,10 +196,6 @@ void mutt_folder_hook (char *path)
   err.dsize = sizeof (buf);
   memset (&token, 0, sizeof (token));
   for (; tmp; tmp = tmp->next)
-  {
-    if(!tmp->command)
-      continue;
-
     if (tmp->type & M_FOLDERHOOK)
     {
       if ((regexec (tmp->rx.rx, path, 0, NULL, 0) == 0) ^ tmp->rx.not)
@@ -213,7 +209,6 @@ void mutt_folder_hook (char *path)
 	}
       }
     }
-  }
   FREE (&token.data);
 }
 
@@ -240,10 +235,6 @@ void mutt_send_hook (HEADER *hdr)
   err.dsize = sizeof (buf);
   memset (&token, 0, sizeof (token));
   for (hook = Hooks; hook; hook = hook->next)
-  {
-    if(!hook->command)
-      continue;
-
     if (hook->type & M_SENDHOOK)
       if ((mutt_pattern_exec (hook->pattern, 0, NULL, hdr) > 0) ^ hook->rx.not)
 	if (mutt_parse_rc_line (hook->command, &token, &err) != 0)
@@ -253,7 +244,6 @@ void mutt_send_hook (HEADER *hdr)
 	  sleep (1);
 	  return;
 	}
-  }
   FREE (&token.data);
 }
 
@@ -264,17 +254,12 @@ mutt_addr_hook (char *path, size_t pathlen, int type, CONTEXT *ctx, HEADER *hdr)
 
   /* determine if a matching hook exists */
   for (hook = Hooks; hook; hook = hook->next)
-  {
-    if(!hook->command)
-      continue;
-
     if (hook->type & type)
       if ((mutt_pattern_exec (hook->pattern, 0, ctx, hdr) > 0) ^ hook->rx.not)
       {
 	mutt_make_string (path, pathlen, hook->command, ctx, hdr);
 	return 0;
       }
-  }
 
   return -1;
 }
