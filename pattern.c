96eb5679bba96bb9067b4ca49c70ab73a9057f6d
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:23:50 +0000

    [project @ 4b33baff9a0c45e9a2c932cf2eedee1ac9112bd2] translation updates
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-05-31 16:10:11+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@838 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- pattern.c
+++ pattern.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1996-8 Michael R. Elkins <me@cs.hmc.edu>
+ * Copyright (C) 1996-9 Michael R. Elkins <me@cs.hmc.edu>, and others
  * 
  *     This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
@@ -143,7 +143,6 @@ msg_search (CONTEXT *ctx, regex_t *rx, char *buf, size_t blen, int op, int msgno
       /* decode the header / body */
       memset (&s, 0, sizeof (s));
       s.fpin = msg->fp;
-      s.flags = M_CHARCONV;
       mutt_mktemp (tempfile);
       if ((s.fpout = safe_fopen (tempfile, "w+")) == NULL)
       {
@@ -1140,10 +1139,10 @@ int mutt_search_command (int cur, int op)
 	return i;
     }
 
-    if (SigInt)
+    if (Signals & S_INTERRUPT)
     {
       mutt_error _("Search interrupted.");
-      SigInt;
+      Signals &= ~S_INTERRUPT;
       return (-1);
     }
 
