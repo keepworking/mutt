2898fd99e08fdb1eb923b99524ab9ea1aef8ecf5
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:26:28 +0000

    [project @ ae384c766175c2839928e419c8f6b1c27d2e3ed1] Dynamically allocate the unget buffer.  Fixes Debian bug #41042. From Gero Treuner <gero@faveve.uni-stuttgart.de>.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-07-26 14:03:37+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@894 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- acconfig.h
+++ acconfig.h
@@ -17,6 +17,9 @@
 /* Does your version of PGP support the PGPPASSFD environment variable? */
 #define HAVE_PGPPASSFD
 
+/* Disable the X-Mailer header? */
+#undef NO_XMAILER
+
 /* What is your domain name? */
 #undef DOMAIN
 
@@ -25,9 +28,6 @@
 
 /* use dotlocking to lock mailboxes? */
 #undef USE_DOTLOCK
-
-/* use an external dotlocking program? */
-#undef DL_STANDALONE
 
 /* use flock() to lock mailboxes? */
 #undef USE_FLOCK
@@ -137,10 +137,3 @@
 
 /* Use the included regex.c? */
 #undef USE_GNU_REGEX
-
-/* Where's mixmaster located? */
-#undef MIXMASTER
-
-/* Where are the character set definitions located? */
-#undef CHARMAPS_DIR
-
