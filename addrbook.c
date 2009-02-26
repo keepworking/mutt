a7dfc8be05e6b010d87fd56d7556c07fe2f686d4
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:35:44 +0000

    [project @ 8cf29159ff3eaecffccd234a92b5cd4293b34f00] post-release commit for 1.0pre4.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-10-11 20:25:03+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@1091 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- addrbook.c
+++ addrbook.c
@@ -67,6 +67,17 @@ alias_format_str (char *dest, size_t destlen, char op, const char *src,
   return (src);
 }
 
+int alias_search (MUTTMENU *m, regex_t *re, int n)
+{
+  char s[LONG_STRING];
+  int slen = sizeof(s);
+
+  mutt_FormatString (s, slen, NONULL (AliasFmt), alias_format_str,
+                    (unsigned long) ((ALIAS **) m->data)[n], 0);
+  return regexec (re, s, 0, NULL, 0);
+}
+
+
 void alias_entry (char *s, size_t slen, MUTTMENU *m, int num)
 {
   mutt_FormatString (s, slen, NONULL (AliasFmt), alias_format_str, (unsigned long) ((ALIAS **) m->data)[num], M_FORMAT_ARROWCURSOR);
@@ -74,7 +85,7 @@ void alias_entry (char *s, size_t slen, MUTTMENU *m, int num)
 
 int alias_tag (MUTTMENU *menu, int n)
 {
-  return ((((ALIAS **) menu->data)[n]->tagged = !((ALIAS **) menu->data)[n]->tagged) ? 1 : -1);
+  return (((ALIAS **) menu->data)[n]->tagged = !((ALIAS **) menu->data)[n]->tagged);
 }
 
 static int alias_SortAlias (const void *a, const void *b)
@@ -126,6 +137,7 @@ void mutt_alias_menu (char *buf, size_t buflen, ALIAS *aliases)
 
   menu = mutt_new_menu ();
   menu->make_entry = alias_entry;
+  menu->search = alias_search;
   menu->tag = alias_tag;
   menu->menu = MENU_ALIAS;
   menu->title = _("Aliases");
