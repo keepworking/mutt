2376f04e565e265dc3e963d93e6288001f548b57
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:28:35 +0000

    [project @ 95b628f837f9478448b6f1a3f2864b5843e36237] resort headers when forward-decoding them with weed switched on.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1999-03-30 21:38:24+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@939 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- parse.c
+++ parse.c
@@ -240,8 +240,6 @@ int mutt_check_mime_type (const char *s)
     return TYPEAUDIO;
   else if (mutt_strcasecmp ("video", s) == 0)
     return TYPEVIDEO;
-  else if (mutt_strcasecmp ("model", s) == 0)
-    return TYPEMODEL;
   else
     return TYPEOTHER;
 }
@@ -833,6 +831,7 @@ void mutt_parse_mime_message (CONTEXT *ctx, HEADER *cur)
 #ifdef _PGPPATH
     cur->pgp = pgp_query (cur->content);
 #endif /* _PGPPATH */
+
 
 
     mx_close_message (&msg);
