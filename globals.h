118623ca266b27eedc8ce7298d489926fcabfc38
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:55:16 +0000

    [project @ 84635ea3dbf67af1fa1868c2959b16b586181699] Fix possible heap corruption.  From Brendan Cully.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 2000-07-17 18:08:27+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@1638 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- globals.h
+++ globals.h
@@ -34,7 +34,6 @@ WHERE char *AttachSep;
 WHERE char *Attribution;
 WHERE char *AttachFormat;
 WHERE char *Charset;
-WHERE char *ComposeFormat;
 WHERE char *DefaultHook;
 WHERE char *DateFmt;
 WHERE char *DsnNotify;
@@ -136,9 +135,9 @@ WHERE short ScoreThresholdRead;
 WHERE short ScoreThresholdFlag;
 
 /* flags for received signals */
-WHERE SIG_ATOMIC_VOLATILE_T SigAlrm INITVAL (0);
-WHERE SIG_ATOMIC_VOLATILE_T SigInt INITVAL (0);
-WHERE SIG_ATOMIC_VOLATILE_T SigWinch INITVAL (0);
+WHERE volatile sig_atomic_t SigAlrm INITVAL (0);
+WHERE volatile sig_atomic_t SigInt INITVAL (0);
+WHERE volatile sig_atomic_t SigWinch INITVAL (0);
 
 WHERE int CurrentMenu;
 
