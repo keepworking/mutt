2311390048ad3787480fb83bb05d3380dee1cbd9
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 22:02:16 +0000

    [project @ b10157d84856040d720e7638abe83fc869ed1282] Documentation for Vikas' latest features.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1998-09-30 06:02:53+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@283 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- pgppubring.c
+++ pgppubring.c
@@ -26,6 +26,7 @@
 #include "sha.h"
 #include "mutt.h"
 #include "pgp.h"
+#include "charset.h"
 
 #define CHUNKSIZE 1024
 
@@ -542,8 +543,6 @@ static KEYINFO *pgp_parse_keyinfo(unsigned char *buff, size_t l)
 static int pgp_parse_pgp2_sig(unsigned char *buff, size_t l, KEYINFO *p)
 {
   unsigned char sigtype;
-  unsigned char pkalg;
-  unsigned char hashalg;
   long sig_gen_time;
   unsigned long signerid;
   size_t j;
@@ -563,9 +562,6 @@ static int pgp_parse_pgp2_sig(unsigned char *buff, size_t l, KEYINFO *p)
   signerid = 0;
   for(i = 0; i < 4; i++)
     signerid = (signerid << 8) + buff[j++];
-  
-  pkalg = buff[j++];
-  hashalg = buff[j++];
   
   if(sigtype == 0x20 || sigtype == 0x28)
     p->flags |= KEYFLAG_REVOKED;
@@ -621,9 +617,9 @@ static int pgp_parse_pgp3_sig(unsigned char *buff, size_t l, KEYINFO *p)
 	if(!--ml) break;
       }
       
-      ml -= skl;
-      if(ml < 0)
+      if((int) ml - (int) skl < 0)
 	break;
+      ml -= skl;
       
       nextone = j + skl;
       skt = buff[j++];
@@ -731,6 +727,7 @@ static KEYINFO *pgp_read_keyring(const char *fname)
   KEYINFO *supkey = NULL;
   PGPUID *uid = NULL;
   LIST **addr = NULL;
+  CHARSET *chs = mutt_get_charset(Charset);
   
   if(!(fp = fopen(fname, "r")))
   {
@@ -795,9 +792,13 @@ static KEYINFO *pgp_read_keyring(const char *fname)
       case PT_NAME:
       {
 	char *chr;
+	
+	if(!addr) break;
+	
 	chr = safe_malloc(l);
 	memcpy(chr, buff + 1, l - 1);
 	chr[l-1] = '\0';
+	mutt_decode_utf8_string(chr, chs);
 	*addr = mutt_new_list();
 	(*addr)->data = safe_malloc(sizeof(PGPUID));
 	uid = (PGPUID *) (*addr)->data;
@@ -835,13 +836,21 @@ KEYINFO *pgp_read_secring(struct pgp_vinfo *pgp)
 void pgp_close_keydb (KEYINFO **ki)
 {
   KEYINFO *tmp, *k = *ki;
-  LIST *q;
+  PGPUID *uid;
+  LIST *p, *q;
   
   while (k)
   {
     if (k->keyid) safe_free ((void **)&k->keyid);
-    for(q = k->address; q; q = q-> next)
+    for(q = k->address; q; q = p)
+    {
+      uid = (PGPUID *) q->data;
+      p = q->next;
+
+      safe_free((void **)&uid->addr);
       safe_free((void **)&q->data);
+      safe_free((void **)&q);
+    }
     tmp = k;
     k = k->next;
     safe_free ((void **)&tmp);
