6170f10ab6318c018af76f9c0b9d907c595118f6
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 21:55:11 +0000

    [project @ d58bceec86c9a6e9bc3b7a7d483aac6d463dd0ca] More roff fixes.
    Original author: Thomas Roessler <roessler@does-not-exist.org>
    Date: 1998-07-15 11:08:17+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@129 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- postpone.c
+++ postpone.c
@@ -94,8 +94,7 @@ static void post_entry (char *s, size_t slen, MUTTMENU *menu, int entry)
 {
   CONTEXT *ctx = (CONTEXT *) menu->data;
 
-  _mutt_make_string (s, slen, NONULL (HdrFmt), ctx, ctx->hdrs[entry],
-		     M_FORMAT_ARROWCURSOR);
+  mutt_make_string (s, slen, NONULL (HdrFmt), ctx, ctx->hdrs[entry]);
 }
 
 static HEADER *select_msg (void)
@@ -242,12 +241,8 @@ int mutt_get_postponed (CONTEXT *ctx, HEADER *hdr, HEADER **cur)
       file[0] = '\0';
       if (b->filename)
 	strfcpy (file, b->filename, sizeof (file));
-      else
-	/* avoid Content-Disposition: header with temporary filename */
-	b->use_disp = 0;
-
-      mutt_adv_mktemp (file, sizeof(file));
-      if (mutt_save_attachment (msg->fp, b, file, 0, NULL) == -1)
+      mutt_adv_mktemp (file);
+      if (mutt_save_attachment (msg->fp, b, file, 0) == -1)
       {
 	mutt_free_envelope (&hdr->env);
 	mutt_free_body (&hdr->content);
@@ -260,7 +255,6 @@ int mutt_get_postponed (CONTEXT *ctx, HEADER *hdr, HEADER **cur)
       b->filename = safe_strdup (file);
       b->unlink = 1;
       mutt_free_body (&b->parts);
-      mutt_stamp_attachment(b);
       b = b->next;
     }
     h->content->parts = NULL;
@@ -268,7 +262,7 @@ int mutt_get_postponed (CONTEXT *ctx, HEADER *hdr, HEADER **cur)
   else
   {
     mutt_mktemp (file);
-    if (mutt_save_attachment (msg->fp, h->content, file, 0, NULL) == -1)
+    if (mutt_save_attachment (msg->fp, h->content, file, 0) == -1)
     {
       mutt_free_envelope (&hdr->env);
       mx_close_message (&msg);
@@ -276,7 +270,7 @@ int mutt_get_postponed (CONTEXT *ctx, HEADER *hdr, HEADER **cur)
       safe_free ((void **) &PostContext);
       return (-1);
     }
-    hdr->content = mutt_make_file_attach (file);
+    hdr->content = mutt_make_attach (file);
     hdr->content->use_disp = 0;	/* no content-disposition */
     hdr->content->unlink = 1;	/* delete when we are done */
   }
