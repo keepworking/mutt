b945f445003d974ef66d959710171d8d035b84a8
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-26 23:28:32 +0000

    [project @ db0199e06fafa6de2832a93483cc77a7f4483583] Document escaping rules for regular expressions (#1351).
    Original author: Michael Elkins <me@sigpipe.org>
    Date: 2002-10-10 09:43:11+00:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@2549 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- hdrline.c
+++ hdrline.c
@@ -26,10 +26,6 @@
 #include "pgp.h"
 #endif
 
-#ifdef HAVE_SMIME
-#include "smime.h"
-#endif
-
 
 
 #include <ctype.h>
@@ -454,8 +450,11 @@ hdr_format_str (char *dest,
 	snprintf (fmt, sizeof (fmt), "%%%sd", prefix);
 	snprintf (dest, destlen, fmt, (int) hdr->lines);
       }
-      else if (hdr->lines <= 0)
-        optional = 0;
+      else
+      {
+	if (hdr->lines > 0)
+	  optional = 0;
+      }
       break;
 
     case 'L':
@@ -493,7 +492,7 @@ hdr_format_str (char *dest,
       }
       else
       {
-	if (hdr->score == 0)
+	if (hdr->score > 0)
 	  optional = 0;
       }
       break;
@@ -624,17 +623,15 @@ hdr_format_str (char *dest,
     
       ch = ' ';
 
-#if defined(HAVE_PGP) || defined(HAVE_SMIME)
-      if (hdr->security & GOODSIGN)
-        ch = 'S';
-      else if (hdr->security & ENCRYPT)
-      	ch = 'P';
-      else if (hdr->security & SIGN)
-        ch = 's';
 #ifdef HAVE_PGP
-      else if (hdr->security & PGPKEY)
+      if (hdr->pgp & PGPGOODSIGN)
+        ch = 'S';
+      else if (hdr->pgp & PGPENCRYPT)
+      	ch = 'P';
+      else if (hdr->pgp & PGPSIGN)
+        ch = 's';
+      else if (hdr->pgp & PGPKEY)
         ch = 'K';
-#endif
 #endif
 
       snprintf (buf2, sizeof (buf2),
