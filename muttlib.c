dfa2318ae464c1078a7f6ed201f7db1ae22e40ad
tonnerre <tonnerre@2bb92213-1a62-4c20-bdc6-606bde718ed5>
2009-02-27 00:31:07 +0000

    [project @ c310dd3d6d7340e8f81e11940e36abeac0de6bdb] Allow reply-hook to use ~h when replying from the index.   Fixes regression introduced by fix for bug #2190.  mutt_pattern_exec() now checks for ctx!=NULL so that Mutt will no longer segfault when replying to a message from the attachment menu.
    Original author: Michael Elkins <me@mutt.org>
    Date: 2007-04-07 16:22:49-07:00
    
    git-svn-id: http://oss.sygroup.ch/svn/mutt/trunk@4407 2bb92213-1a62-4c20-bdc6-606bde718ed5

--- muttlib.c
+++ muttlib.c
@@ -1602,8 +1602,7 @@ void mutt_buffer_add (BUFFER* buf, const char* s, size_t len)
   {
     offset = buf->dptr - buf->data;
     buf->dsize += len < 128 ? 128 : len + 1;
-    /* suppress compiler aliasing warning */
-    safe_realloc ((void**) (void*) &buf->data, buf->dsize);
+    safe_realloc ((void**) &buf->data, buf->dsize);
     buf->dptr = buf->data + offset;
   }
   memcpy (buf->dptr, s, len);
